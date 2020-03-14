5ca9c4321ff776b3ad09958f295d0771f7101edc
sbx_user1051 <sbx_user1051@169.254.105.37>
2020-03-14 21:58:19 +0000

    Sat Mar 14 21:58:19 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,5 +1,7 @@
 package befaster.solutions.CHK;
 
+import com.google.common.collect.ImmutableMap;
+
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Objects;
@@ -8,8 +10,14 @@ public class CheckoutSolution {
     private Map<String, Product> skuPrices = new Hashtable<>();
 
     public CheckoutSolution() {
-        skuPrices.put("A", new Product("A", 50));
-        skuPrices.put("B", new Product("B", 45));
+        Product productA = new Product("A", 50);
+        productA.setOffers(ImmutableMap.of(3, 130));
+        skuPrices.put("A", productA);
+
+        Product productB = new Product("B", 45);
+        productB.setOffers(ImmutableMap.of(2, 45));
+        skuPrices.put("B", productB);
+
         skuPrices.put("C", new Product("C", 20));
         skuPrices.put("D", new Product("D", 15));
     }
@@ -28,5 +36,6 @@ public class CheckoutSolution {
         return total;
     }
 }
+
 
 
