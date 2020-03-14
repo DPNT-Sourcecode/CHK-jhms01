3269b107502133d2a13d9f88636ccda48886f84d
sbx_user1051 <sbx_user1051@169.254.113.109>
2020-03-14 21:03:08 +0000

    Sat Mar 14 21:03:08 UTC 2020

---
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -0,0 +1,25 @@
+package befaster.solutions.HLO;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Asif Akhtar
+ * 14/03/2020 21:01
+ */
+public class HelloSolutionTest {
+    private HelloSolution helloSolution;
+
+    @Before
+    public void setup() {
+        helloSolution = new HelloSolution();
+    }
+
+    @Test
+    public void should_return_simple_hello_message() {
+        assertThat(helloSolution.hello("Fred"), is("Hello"));
+    }
+}
