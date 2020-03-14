f5eaef501d39574e9a9063f89e065efda042573c
sbx_user1051 <sbx_user1051@169.254.105.37>
2020-03-14 22:04:19 +0000

    Sat Mar 14 22:04:19 UTC 2020

--- src/main/java/befaster/solutions/CHK/Ordered.java
+++ src/main/java/befaster/solutions/CHK/Ordered.java
@@ -5,4 +5,31 @@ package befaster.solutions.CHK;
  * 14/03/2020 22:00
  */
 public class Ordered {
+    private String sku;
+    private int ordered;
+
+    public Ordered(String sku) {
+        this.sku = sku;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public int getOrdered() {
+        return ordered;
+    }
+
+    public void setOrdered(int ordered) {
+        this.ordered = ordered;
+    }
+
+    public void add() {
+        this.ordered += 1;
+    }
 }
+
